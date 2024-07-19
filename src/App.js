395f870e046d06b4d0b48560ace53bdf6a2dfcd1
Makcim Motruk <56037758+MakcFaktor@users.noreply.github.com>
2024-07-19 16:41:00 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <h1 className="text-center mb-4">SWAPI Interface</h1>
+      <div className="row">
+        <div className="col-md-4">
+          <h2>Characters</h2>
+          <ul className="list-group">
+            <li className="list-group-item">Luke Skywalker</li>
+            <li className="list-group-item">Darth Vader</li>
+            <li className="list-group-item">Leia Organa</li>
+          </ul>
+        </div>
+        <div className="col-md-4">
+          <h2>Planets</h2>
+          <ul className="list-group">
+            <li className="list-group-item">Tatooine</li>
+            <li className="list-group-item">Alderaan</li>
+            <li className="list-group-item">Hoth</li>
+          </ul>
+        </div>
+        <div className="col-md-4">
+          <h2>Starships</h2>
+          <ul className="list-group">
+            <li className="list-group-item">Millennium Falcon</li>
+            <li className="list-group-item">X-wing</li>
+            <li className="list-group-item">TIE Fighter</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
